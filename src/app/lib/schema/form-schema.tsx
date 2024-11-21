fa5e45cb6c7ac390df95ce1a052c8aaf9338701b
osdouadi <osdouadi@gmail.com>
2024-11-21 23:55:11 +0100

    Update

---
+++ src/app/lib/schema/form-schema.tsx
@@ -0,0 +1,12 @@
+import * as z from "zod";
+
+export const useFormSchema = () => {
+  return z.object({
+    userType: z.string().optional(),
+    fullName: z.string().min(2, { message: "Debe ingresar el nombre" }),
+    email: z.string().optional(),
+    phoneNumber: z.string().optional(),
+    contentType: z.string().optional(),
+    content: z.string().min(1, { message: "Debe escribir el contenido" }),
+  });
+};
